12e6343cf55e289bdc7b8c1d1771ba0924887267
Filippos Vasilakis <vasilakisfil@gmail.com>
2015-03-04 17:05:36 +0100

    Adds more options (api_url, release, link_resolver etc)

--- lib/middleman_prismic.rb
+++ lib/middleman_prismic.rb
@@ -9,12 +9,35 @@ module MiddlemanPrismic
   class Core < Middleman::Extension
 
     option :api_url, nil, 'The prismic api url'
-    option :release
+    option :release, 'master', 'Content release'
+    option(
+      :link_resolver,
+      ->(link) {"http://www.example.com/#{link.type.pluralize}/#{link.slug}"},
+      'The link resolver'
+    )
+    option :custom_queries, {}, 'Custom queries'
 
     def initialize(app, options_hash={}, &block)
       super
 
       MiddlemanPrismic.instance_variable_set('@options', options)
+    end
+
+    helpers do
+      Dir["data/prismic_*"].each do |file|
+        define_method(file.gsub('data/prismic_','')) do
+          YAML::load(File.read(file))
+        end
+      end
+
+      def reference
+        ref = YAML::load(File.read('data/prismic_reference'))
+        ref.class.send(
+          :define_method, :link_to, MiddlemanPrismic.options.link_resolver
+        )
+
+        return ref
+      end
     end
   end
 
--- lib/middleman_prismic/commands/prismic.rb
+++ lib/middleman_prismic/commands/prismic.rb
@@ -12,9 +12,11 @@ module Middleman
       namespace :prismic
       desc 'prismic', 'Import data from Prismic'
 
+=begin
       method_option "refetch",
         aliases: "-r",
         desc: "Refetches the data from Prismic"
+=end
 
       def self.source_root
         ENV['MM_ROOT']
@@ -27,9 +29,12 @@ module Middleman
 
       def prismic
         ::Middleman::Application.server.inst
+        reference = MiddlemanPrismic.options.release
+
+        FileUtils.rm_rf(Dir.glob('data/prismic_*'))
 
         api = ::Prismic.api(MiddlemanPrismic.options.api_url)
-        response = api.form('everything').submit(api.master_ref)
+        response = api.form('everything').submit(api.ref(reference))
 
         available_documents = []
         response.each {|d| available_documents << d.type}
@@ -38,11 +43,21 @@ module Middleman
 
         available_documents.each do |document_type|
           documents = response.select{|d| d.type == document_type}
-          File.open("data/#{document_type.pluralize}", 'w') do |f|
+          File.open("data/prismic_#{document_type.pluralize}", 'w') do |f|
             f.write(Hash[[*documents.map.with_index]].invert.to_yaml)
           end
         end
 
+        File.open('data/prismic_reference', 'w') do |f|
+          f.write(api.master_ref.to_yaml)
+        end
+
+        MiddlemanPrismic.options.custom_queries.each do |k, v|
+          response = api.form('everything').query(*v).submit(api.master_ref)
+          File.open("data/prismic_custom_#{k}", 'w') do |f|
+            f.write(Hash[[*response.map.with_index]].invert.to_yaml)
+          end
+        end
       end
 
     end
